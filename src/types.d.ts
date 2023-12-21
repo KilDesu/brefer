668e160fd379cd3d3876556401af878b82af29a1
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-12-21 21:47:31 +0100

    Add types definitions

---
+++ src/types.d.ts
@@ -0,0 +1,34 @@
+declare namespace Bref {
+	interface PreprocessorOptions {
+		prefix?: string;
+	}
+
+	interface Position {
+		start: number;
+		end: number;
+	}
+
+	interface ReactiveValue extends Position {
+		name: string;
+	}
+
+	interface DerivedValue extends ReactiveValue {
+		dependencies: string[];
+	}
+
+	interface Options {
+		prefix: string;
+		REACTIVE_VALUES: Bref.ReactiveValue[];
+		DERIVED_VALUES: Bref.DerivedValue[];
+	}
+
+	type Node = import("estree-walker").Node & Position;
+	type VariableDeclaration = Bref.Node & { type: "VariableDeclaration" };
+	type VariableDeclarator = Bref.Node & { type: "VariableDeclarator" };
+	type Identifier = Bref.Node & { type: "Identifier" };
+
+	type Expression = import("estree").Expression & Position;
+	type ArrayPattern = import("estree").ArrayPattern & Position;
+	type ArrayExpression = import("estree").ArrayExpression & Position;
+	type ClassBody = import("estree").ClassBody & Position;
+}
