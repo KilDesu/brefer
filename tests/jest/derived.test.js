967ada50f11676ee52f98a6c9c69ddd44d6dfaeb
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-12-21 21:54:56 +0100

    Add tests for derived and state preprocessing

---
+++ tests/jest/derived.test.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import { preprocess } from "svelte/compiler";
+import BrefPreprocessor from "../../src/index.js";
+
+const path = "tests/fixtures/derived";
+
+describe("Derived", () => {
+	test("Function with parameter", async () => {
+		const file = "functionWithParam.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tfunction double(count) {\r\n\t\treturn count * 2;\r\n\t}\r\n\r\n\tlet $count = $state(0);\r\n\tlet $double = $derived(double($count));\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+
+	test("Literal expression", async () => {
+		const file = "literalExpression.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tlet $count = $state(0);\r\n\r\n\tlet $double = $derived($count * 2);\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+
+	test("Array destructuring", async () => {
+		const file = "arrayDestructuring.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tfunction pow2(x) {\r\n\t\treturn x * x;\r\n\t}\r\n\r\n\tlet [$foo, $bar] = [$state(1), $state(2)];\r\n\r\n\tlet [[$doubleFoo, $tripleFoo], $powBar] = [\r\n\t\t[$derived($count * 2), $derived($count * 3)],\r\n\t\t$derived(pow2($bar)),\r\n\t];\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+});
---
+++ tests/jest/state.test.js
@@ -0,0 +1,59 @@
+import fs from "fs";
+import { preprocess } from "svelte/compiler";
+import BrefPreprocessor from "../../src/index.js";
+
+const path = "tests/fixtures/state";
+
+describe("State", () => {
+	test("No initial value", async () => {
+		const file = "noInitialValue.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tlet $count = $state();\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+
+	test("With initial value", async () => {
+		const file = "initialValue.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tlet notReactive = true;\r\n\r\n\tlet $foo = $state("bar");\r\n\r\n\tlet $count = $state(0);\r\n\r\n\tlet $object = $state({\r\n\t\tfoo: "bar",\r\n\t\tbaz: "qux",\r\n\t});\r\n\r\n\tlet $array = $state([1, 2, 3, 4, 5]);\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+
+	test("Class property declarations", async () => {
+		const file = "classProperty.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tclass Foo {\r\n\t\t$foo = $state(1);\r\n\t\tbar = 2;\r\n\t\t$baz = $state();\r\n\t}\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+
+	test("Array destructuring", async () => {
+		const file = "arrayDestructuring.svelte";
+		const content = fs.readFileSync(`${path}/${file}`, "utf8");
+
+		const preprocessed = await preprocess(content, BrefPreprocessor(), {
+			filename: file,
+		});
+
+		const expectedCode = `<script>\r\n\tlet [$foo, bar, $baz] = [$state(1), 2, $state(3)];\r\n\r\n\tlet [[$a, b], $c] = [[$state(1), 2], $state(3)];\r\n</script>\r\n`;
+
+		expect(preprocessed.code).toStrictEqual(expectedCode);
+	});
+});
