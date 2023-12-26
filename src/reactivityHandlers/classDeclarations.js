04e67672794ea2b9ea679fc369f3723bf754d59e
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-12-26 22:48:13 +0100

    Refactor class property declarations to use reactive and derived values

--- src/reactivityHandlers/classDeclarations.js
+++ src/reactivityHandlers/classDeclarations.js
@@ -5,31 +5,34 @@ import { isIdentifier } from "../utils.js";
  * Handles class property declarations.
  *
  * @export
- * @param {Bref.ClassBody} node - The class body node.
- * @param {Bref.Options} options - Bref options, containing the prefix and the arrays which store the reactive and derived values
+ * @param {Brefer.ClassBody} node - The class body node.
+ * @param {Brefer.Context} ctx - Bref context, containing the prefix and the arrays which store the reactive and derived values as well as the untrack calls
  * @example
  * ```js
  * class Foo {
- *   $foo = "bar";
- *   $baz;
+ *   r$foo = "bar";
+ *   r$baz;
+ *   r$qux = `Hello, ${r$foo}!`
  * }
  * ```
  * Will result in:
  * ```js
  * class Foo {
- *  $foo = $state("bar");
- *  $baz = $state();
+ *   r$foo = $state("bar");
+ *   r$baz = $state();
+ *   r$qux = $derived(`Hello, ${r$foo}!`);
  * }
+ * ```
  */
-export function handleClassDeclarations(node, options) {
+export function handleClassDeclarations(node, ctx) {
 	for (const property of node.body) {
 		if (property.type !== "PropertyDefinition") continue;
 
 		if (isIdentifier(property.key)) {
 			handleIdentifier(
 				property.key,
-				/** @type {Bref.Expression | null | undefined} */ (property.value),
-				options
+				/** @type {Brefer.Expression | null | undefined} */ (property.value),
+				ctx
 			);
 		}
 	}
