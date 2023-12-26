fe3ebe4aa29f1ceb99fdaf1289647293bf01f0b5
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-12-26 22:48:47 +0100

    Add cleanup utility function for prefix replacement

---
+++ src/reactivityUtils/cleanup.js
@@ -0,0 +1,33 @@
+import { walk } from "estree-walker";
+import MagicString from "magic-string";
+import { isIdentifier } from "../utils.js";
+
+/**
+ * Replaces all the prefixes in front of the state and derived variables by the default prefix `r$`.
+ *
+ * This is important in case the user wants to use `$` as the prefix, because Svelte uses `$` to subscribe to stores.
+ *
+ * @export
+ * @param {Brefer.Node} ast - The AST of the source code
+ * @param {MagicString} source - The source code as a MagicString instance
+ * @param {Brefer.Context} ctx - Bref context, containing the prefix and the arrays which store the reactive and derived values as well as the untrack calls
+ */
+export function cleanup(ast, source, ctx) {
+	walk(ast, {
+		enter(node) {
+			if (isIdentifier(node)) {
+				const isReactiveIdentifier = ctx.REACTIVE_VALUES.concat(
+					ctx.DERIVED_VALUES
+				).some((value) => value.name === node.name);
+
+				if (isReactiveIdentifier && ctx.prefix === "$") {
+					source.update(
+						node.start,
+						node.end,
+						node.name.replace(ctx.prefix, "r$")
+					);
+				}
+			}
+		},
+	});
+}
