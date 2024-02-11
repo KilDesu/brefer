64f98315eb94f5a36b4f867695914f3dbd522f8d
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-02-11 18:41:55 +0100

    Update dependencies and remove unused Vite config

--- packages/vite-plugin-svelte/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-import { brefer } from "./src/index.js";
-
-export default defineConfig({
-	plugins: [brefer()],
-	test: {
-		include: ["src/**/*"],
-	},
-});
