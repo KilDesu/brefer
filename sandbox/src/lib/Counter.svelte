0b33badcdc78eccc1f3b8ed159fa0d4459b906db
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-02-26 22:42:43 +0100

    Add sandox to test the plugin

---
+++ sandbox/src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Counter } from "./test.svelte.js";
+
+	let counter = new Counter();
+
+	$$(() => {
+		console.log(counter.count, counter.double);
+	});
+</script>
+
+<button on:click={() => counter.increment()}>
+	count is {counter.count}, double is {counter.double}
+</button>
---
+++ sandbox/src/lib/test.svelte.js
@@ -0,0 +1,8 @@
+export class Counter {
+	count = 0;
+	double = $(this.count * 2);
+
+	increment() {
+		this.count++;
+	}
+}
